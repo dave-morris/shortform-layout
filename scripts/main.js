f228afac3f04c10f966113019728e2793d70aeca
dave-morris <david.morris@bbc.co.uk>
2019-09-30 15:48:03 +0100

    tweaks to pinning and layouts

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,45 @@
-// get height of video videoContainer
-const videoHeight = document.getElementById("topVideo").clientHeight;
-document.getElementById("actualVideo").style.height = videoHeight;
-// console.log(videoHeight);
+if (document.getElementById('mediaContainer').classList.contains("one-column")) {
+
+  //do nothing
+
+} else {
+
+  // get height of video videoContainer
+  const videoHeight = document.getElementById("videoContainer").clientHeight;
+  console.log("The height of the video is " + videoHeight);
+  // document.getElementById("actualVideo").style.height = videoHeight;
+
+  document.getElementById('mediaContainer').style.top = videoHeight + "px";
+
+};
 
 window.onresize = function(event) {
-  const videoHeight = document.getElementById("topVideo").clientHeight;
-  // console.log(videoHeight);
 
-  document.getElementById("actualVideo").style.height = videoHeight;
+  const videoHeight = document.getElementById("videoContainer").clientHeight;
 
+  if (document.getElementById('mediaContainer').classList.contains("one-column")) {
+
+    //Do nothing
+
+  } else {
+
+    // document.getElementById("actualVideo").style.height = videoHeight;
+
+  }
 };
 
 
 
-// get the sticky element
-
-// const stickyElm = document.getElementById('videoContainer')
-//
-// const observer = new IntersectionObserver(
-//
-//     ([e]) => e.target.classList.toggle('media__image__maxHeight', e.intersectionRatio < 1),
-//     {threshold: [1]}
-//
-// );
-//
-// observer.observe(stickyElm)
 
 
-
-window.onscroll = function() {myFunction()};
+window.onscroll = function() { myFunction() };
 
 function myFunction() {
 
   var element = document.getElementById("scrollPosition");
   var elementTwo = document.getElementById("videoContainer");
 
-  console.log(document.documentElement.scrollTop);
+  // console.log(document.documentElement.scrollTop);
 
   if (document.documentElement.scrollTop > 80) {
 
@@ -44,7 +48,27 @@ function myFunction() {
   } else if (document.documentElement.scrollTop == 0) {
 
     elementTwo.classList.remove("media__image__maxHeight");
-    console.log(document.documentElement.scrollTop);
+    // console.log(document.documentElement.scrollTop);
 
   }
+
+  if (document.getElementById('mediaContainer').classList.contains("one-column")) {
+
+    //Do nothing
+
+  } else {
+
+    // get height of video videoContainer
+    const videoHeight = document.getElementById("videoContainer").clientHeight;
+    // console.log("The height of the video is " + videoHeight);
+    // document.getElementById("actualVideo").style.height = videoHeight;
+
+    document.getElementById('mediaContainer').style.top = videoHeight + "px";
+
+    window.onresize = function(event) {
+      const videoHeight = document.getElementById("videoContainer").clientHeight;
+    };
+
+  }
+
 }
