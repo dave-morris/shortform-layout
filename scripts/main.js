b0c201a5cc7fa78ee74b1aaec0c761daa47da098
dave-morris <david.morris@bbc.co.uk>
2019-09-30 17:02:45 +0100

    cleaning up a few things

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,15 @@
+
 if (document.getElementById('mediaContainer').classList.contains("one-column")) {
-
   //do nothing
-
 } else {
-
   // get height of video videoContainer
   const videoHeight = document.getElementById("videoContainer").clientHeight;
-  console.log("The height of the video is " + videoHeight);
-  // document.getElementById("actualVideo").style.height = videoHeight;
-
   document.getElementById('mediaContainer').style.top = videoHeight + "px";
-
 };
 
 window.onresize = function(event) {
-
   const videoHeight = document.getElementById("videoContainer").clientHeight;
-
-  if (document.getElementById('mediaContainer').classList.contains("one-column")) {
-
-    //Do nothing
-
-  } else {
-
-    // document.getElementById("actualVideo").style.height = videoHeight;
-
-  }
 };
-
-
-
 
 
 window.onscroll = function() { myFunction() };
@@ -39,8 +19,6 @@ function myFunction() {
   var element = document.getElementById("scrollPosition");
   var elementTwo = document.getElementById("videoContainer");
 
-  // console.log(document.documentElement.scrollTop);
-
   if (document.documentElement.scrollTop > 80) {
 
     elementTwo.classList.add("media__image__maxHeight");
@@ -48,7 +26,6 @@ function myFunction() {
   } else if (document.documentElement.scrollTop == 0) {
 
     elementTwo.classList.remove("media__image__maxHeight");
-    // console.log(document.documentElement.scrollTop);
 
   }
 
